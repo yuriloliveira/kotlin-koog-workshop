c38d67c36ce48de6886406924625e3a88d57ae0a
yuriloliveira <yuri12194@gmail.com>
2025-09-07 14:40:41 +0200

    Moves agent that creates file to a new file.

--- src/main/kotlin/AgentMCPApplication.kt
+++ src/main/kotlin/AgentMCPApplication.kt
@@ -1,34 +1,10 @@
-import ai.koog.agents.core.agent.AIAgent
-import ai.koog.agents.mcp.McpToolRegistryProvider
-import ai.koog.prompt.executor.llms.all.simpleOllamaAIExecutor
-import ai.koog.prompt.llm.OllamaModels
+import agents.CreateFileAgent
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     runBlocking {
-        val fileSystemMCPProcess = ProcessBuilder(
-            "docker",
-            "run",
-            "-i",
-            "--rm",
-            "-v",
-            "./ai-assistant:/ai",
-            "mcp/filesystem",
-            "/ai"
-        ).start()
-
-        val toolRegistry = McpToolRegistryProvider.fromTransport(
-            transport = McpToolRegistryProvider.defaultStdioTransport(fileSystemMCPProcess)
-        )
-
-        val agent = AIAgent(
-            executor = simpleOllamaAIExecutor(),
-            llmModel = OllamaModels.Meta.LLAMA_3_2_3B,
-            toolRegistry = toolRegistry,
-        )
-
-        agent.run("""Create a file movie-recommendation.txt in folder '/ai'
-            | where the content is a random funny sentence.""".trimMargin()
-        )
+        CreateFileAgent()
+            .agent
+            .run("Create a file movie-recommendation.txt with content 'dummy content'.")
     }
 }
---
+++ src/main/kotlin/agents/CreateFileAgent.kt
@@ -0,0 +1,42 @@
+package agents
+
+import ai.koog.agents.core.agent.AIAgent
+import ai.koog.agents.features.eventHandler.feature.EventHandler
+import ai.koog.agents.mcp.McpToolRegistryProvider
+import ai.koog.prompt.executor.llms.all.simpleOllamaAIExecutor
+import ai.koog.prompt.llm.OllamaModels
+import kotlinx.coroutines.runBlocking
+
+class CreateFileAgent {
+    val agent: AIAgent<String, String> = runBlocking {
+        val fileSystemMCPProcess = ProcessBuilder(
+            "docker",
+            "run",
+            "-i",
+            "--rm",
+            "-v",
+            "./ai-assistant:/ai",
+            "mcp/filesystem",
+            "/ai"
+        ).start()
+
+        val toolRegistry = McpToolRegistryProvider.fromTransport(
+            transport = McpToolRegistryProvider.defaultStdioTransport(fileSystemMCPProcess)
+        )
+
+        AIAgent(
+            executor = simpleOllamaAIExecutor(),
+            llmModel = OllamaModels.Meta.LLAMA_3_2_3B,
+            toolRegistry = toolRegistry,
+            systemPrompt = """You are a file manager that creates files under folder /ai.
+                | You create the folder in case it doesn't exist.""".trimMargin(),
+            installFeatures = {
+                install(EventHandler) {
+                    onToolCall {
+                        println("Tool called ${it.tool.name} with args ${it.toolArgs}")
+                    }
+                }
+            }
+        )
+    }
+}
